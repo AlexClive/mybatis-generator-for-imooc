064b3cfbc59ba0aba1712ad4b41c235154efa30f
zhangxuerong <1027092674@qq.com>
2023-02-24 17:12:19 +0800

    Initial commit

---
+++ src/main/java/com/zhangxuerong/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zhangxuerong.mapper;
+
+import com.zhangxuerong.my.mapper.MyMapper;
+import com.zhangxuerong.pojo.User;
+
+public interface UserMapper extends MyMapper<User> {
+}
---
+++ src/main/java/com/zhangxuerong/pojo/User.java
@@ -0,0 +1,194 @@
+package com.zhangxuerong.pojo;
+
+import java.util.Date;
+import javax.persistence.*;
+
+public class User {
+    /**
+     * 用户id
+     */
+    @Column(name = "user_id")
+    private String userId;
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 用户名
+     */
+    @Column(name = "user_name")
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 头像
+     */
+    private String photo;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    /**
+     * 获取用户id
+     *
+     * @return user_id - 用户id
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * 设置用户id
+     *
+     * @param userId 用户id
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * 获取手机号
+     *
+     * @return mobile - 手机号
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * 设置手机号
+     *
+     * @param mobile 手机号
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @return user_name - 用户名
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param userName 用户名
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取头像
+     *
+     * @return photo - 头像
+     */
+    public String getPhoto() {
+        return photo;
+    }
+
+    /**
+     * 设置头像
+     *
+     * @param photo 头像
+     */
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    /**
+     * 获取邮箱
+     *
+     * @return email - 邮箱
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置邮箱
+     *
+     * @param email 邮箱
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取创建时间
+     *
+     * @return create_time - 创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param createTime 创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * 获取更新时间
+     *
+     * @return update_time - 更新时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * 设置更新时间
+     *
+     * @param updateTime 更新时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
